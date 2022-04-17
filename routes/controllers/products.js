d0caeaea6a23aaec697850d427beccfc18a450f6
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-04-17 20:00:07 +0900

    new best sale

--- routes/controllers/products.js
+++ routes/controllers/products.js
@@ -2,7 +2,24 @@ const { productNew, productBest, productSale } = require("../../models");
 // const authMiddleware = require("../middleswares/auth-middleware")
 
 const newProduct = async (req,res) => {
-    const { page } = req.params
     const newList = await productNew.findAll({ where: {} })
-    console.log();
+    console.log(newList);
+    res.json({newList})
 }
+
+const bestProduct = async (req,res) => {
+    const bestList = await productBest.findAll({ where: {} })
+    console.log(bestList);
+    res.json({bestList})
+}
+
+const saleProduct = async (req,res) => {
+    const saleList = await productSale.findAll({ where: {} })
+    console.log(saleList);
+    res.json({saleList})
+}
+
+
+module.exports = { newProduct, bestProduct, saleProduct }
+
+
