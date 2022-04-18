6ba3b9256ada909bb6d7e29b709a9960e39c9b90
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-04-18 19:42:12 +0900

    cart 추가

--- routes/cart.js
+++ routes/cart.js
@@ -2,19 +2,19 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/auth-middleware");
 
-const controller = require("./controllers/carts");
+const {addCart} = require("./controllers/carts");
 
 
 
 
 // 장바구니 추가
-router.post("/:cart", authMiddleware);
+router.post("/cart", authMiddleware, addCart);
 
 // 장바구니 조회
 router.get("/cart/:productbestId", authMiddleware);
 
 // 장바구니 삭제
-router.delete("/cart/delete", authMiddleware);
+router.delete("/cart", authMiddleware);
 
 
 
