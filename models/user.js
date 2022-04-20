4f95d333ecdfa8c140c667b551c0f4bb581327d0
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-04-21 02:43:15 +0900

    test

--- models/user.js
+++ models/user.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -26,5 +24,6 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'User',
   });
+  
   return User;
 };
